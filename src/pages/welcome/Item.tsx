99465d56324230162113724e3521ff876700af8d
Andrius Adomaitis <andrius.adom@gmail.com>
2023-03-22 11:03:44 +0200

    #S2SG-16 removed content component, transfered everything to pages/welcome

--- src/pages/welcome/Item.tsx
+++ src/pages/welcome/Item.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Content.module.css';
+import styles from './WelcomePage.module.css';
 
 const Item = (props: { icon2?: string; icon1: string; title: string }) => {
   return (
--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import Content from '../../components/content/Content';
+import { Grid } from '@mui/material';
+import styles from './WelcomePage.module.css';
+import Item from './Item';
+import mannageApointmentIcon from '../../assets/manage-appointments-icon.svg';
+import physicianTimetablesIcon from '../../assets/physician-timetables-icon.svg';
+import manageUsersIcon from '../../assets/manage-users-icon.svg';
+import managePhysiciansIcon from '../../assets/manage-physicians-icon.svg';
 
 const WelcomePage = () => {
-  return <Content />;
+  return (
+    <Grid
+      className={styles.main}
+      container
+      direction='row'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Item
+        title='Manage users'
+        icon1={manageUsersIcon}
+        icon2={managePhysiciansIcon}
+      ></Item>
+      <Item
+        title='Physicians timetables'
+        icon1={physicianTimetablesIcon}
+      ></Item>
+      <Item title='Manage appointments' icon1={mannageApointmentIcon}></Item>
+    </Grid>
+  );
 };
 
 export default WelcomePage;

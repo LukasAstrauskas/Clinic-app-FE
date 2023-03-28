fd6d7984e9e1c47bcb76ab4de20ccfca5d1e397d
Andrius Adomaitis <andrius.adom@gmail.com>
2023-03-28 11:48:29 +0300

    npm run lint:fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WelcomePage from './pages/welcome/WelcomePage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Login from './pages/login/Login';
 import Container from '@mui/material/Container';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Container maxWidth='lg'>
       <Header />
+      {/* <Login /> */}
       <WelcomePage />
       <Footer />
     </Container>

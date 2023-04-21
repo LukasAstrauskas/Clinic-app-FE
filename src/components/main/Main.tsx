6bde72b20ecf94b43abfabcae606b84bc83e9db4
Andrius Adomaitis <andrius.adom@gmail.com>
2023-04-21 15:27:30 +0300

    added PrivateRoutes, to prevent not logged users watch content

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,17 +9,20 @@ import NotFound from '../../pages/notFound/NotFound';
 import UserTabs from '../../pages/users/Tabs';
 import TimetablesContainer from '../../pages/timetables/TimetablesContainer';
 import BookAppointment from '../../pages/admin/BookAppointment';
+import PrivateRoutes from '../../routes/PrivateRoutes';
 
 const Main = () => {
   return (
     <Routes>
-      <Route path='/' element={<WelcomePage />} />
-      <Route path={ROUTES.USERS} element={<UserTabs />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
-      <Route path={ROUTES.PATIENTS} element={<Patients />} />
-      <Route path={ROUTES.TIMETABLE} element={<TimetablesContainer />} />
-      <Route path={ROUTES.BOOKAPPOINTMENT} element={<BookAppointment />} />
       <Route path='*' element={<NotFound />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path='/' element={<WelcomePage />} />
+        <Route path={ROUTES.USERS} element={<UserTabs />} />
+        <Route path={ROUTES.PATIENTS} element={<Patients />} />
+        <Route path={ROUTES.TIMETABLE} element={<TimetablesContainer />} />
+        <Route path={ROUTES.BOOKAPPOINTMENT} element={<BookAppointment />} />
+      </Route>
     </Routes>
   );
 };

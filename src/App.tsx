8c07aaf4927fdfd646dbc40b54c389835f944448
Mykolas <mykolassanda@gmai.com>
2023-04-27 20:17:24 +0300

    final commit with fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
-import Tabs from './pages/users/Tabs';
 
 function App() {
   return (

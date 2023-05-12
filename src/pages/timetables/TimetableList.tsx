4a25f657216c29abd394431428e3d0f8d6047a05
Andrius Adomaitis <andrius.adom@gmail.com>
2023-05-12 16:33:23 +0300

    fixed issue S2SG-82, not creating new PR

--- src/pages/timetables/TimetableList.tsx
+++ src/pages/timetables/TimetableList.tsx
@@ -215,7 +215,11 @@ const TimetableList = ({ physicianId }: Props) => {
                       ></Chip>
                     </TableCell>
                     <TableCell align='left'>
-                      <Stack direction='row' spacing={'0.4%'}>
+                      <Stack
+                        direction='row'
+                        spacing={'0.4%'}
+                        style={{ flexWrap: 'wrap' }}
+                      >
                         {timePatientList.map(({ time, patientId }) => {
                           return (
                             <>

aa3bcf4f4ad65758b71d2a45a43c8c96f333b950
Lukas <lukas.astrauskas@gmail.com>
2023-10-25 12:16:42 +0300

    Header.tsx cleared

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
 import clinicLogo from '../../assets/clinic-logo.svg';
-import { Avatar, Button, Menu, MenuItem, Stack } from '@mui/material';
+import { Avatar, IconButton, Menu, MenuItem, Stack } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../routes/routes';
@@ -14,23 +14,26 @@ import {
 } from '../../store/slices/loggedUser/loggedUserSlice';
 
 const Header = () => {
+  const loggedUser = useAppSelector(selectLoggedUser);
+  const isLogged = useAppSelector(selectIsLogged);
+  const dispatch = useAppDispatch();
+  const navige = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const openMenu = Boolean(anchorEl);
+
   const handleClickAvatar = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
-  const navige = useNavigate();
-
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const openMenu = Boolean(anchorEl);
 
   const handleCloseMenu = () => {
     setAnchorEl(null);
   };
 
-  const dispatch = useAppDispatch();
-
-  const loggedUser = useAppSelector(selectLoggedUser);
-  const isLogged = useAppSelector(selectIsLogged);
+  const handelLogout = () => {
+    handleCloseMenu();
+    dispatch(logout());
+    dispatch(resetStore());
+  };
 
   return (
     <div className={styles.header}>
@@ -45,43 +48,27 @@ const Header = () => {
         </div>
       </NavLink>
       <Stack direction='row' spacing={2}>
-        <div>
-          {isLogged && (
-            <>
-              <Button onClick={handleClickAvatar}>
-                <Avatar className={styles.avatar} sx={{ bgcolor: grey[100] }}>
-                  <div className={styles.avatarLogo}>{loggedUser.initials}</div>
-                </Avatar>
-              </Button>
-              <Menu
-                open={openMenu}
-                anchorEl={anchorEl}
-                onClose={handleCloseMenu}
-              >
-                {loggedUser.type === 'patient' && (
-                  <MenuItem
-                    onClick={() => {
-                      navige(ROUTES.PATIENTPROFILE);
-                      handleCloseMenu();
-                    }}
-                  >
-                    Profile
-                  </MenuItem>
-                )}
-                <MenuItem
-                  onClick={() => {
-                    handleCloseMenu();
-                    dispatch(logout());
-                    dispatch(resetStore());
-                  }}
-                >
-                  Log-out
-                </MenuItem>
-              </Menu>
-            </>
-          )}
-        </div>
+        {isLogged && (
+          <IconButton onClick={handleClickAvatar} disableRipple>
+            <Avatar className={styles.avatar} sx={{ bgcolor: grey[100] }}>
+              <div className={styles.avatarLogo}>{loggedUser.initials}</div>
+            </Avatar>
+          </IconButton>
+        )}
       </Stack>
+      <Menu open={openMenu} anchorEl={anchorEl} onClose={handleCloseMenu}>
+        {loggedUser.type === 'patient' && (
+          <MenuItem
+            onClick={() => {
+              navige(ROUTES.PATIENTPROFILE);
+              handleCloseMenu();
+            }}
+          >
+            Profile
+          </MenuItem>
+        )}
+        <MenuItem onClick={handelLogout}>Log-out</MenuItem>
+      </Menu>
     </div>
   );
 };
